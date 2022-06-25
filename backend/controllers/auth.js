32a8869184204d2d9fa65d15e716b56e1d89a64e
Denish-Ranpariya <ranpariyadenish1512@gmail.com>
2022-06-26 00:38:20 +0530

    added jwt for authentication.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,9 +1,12 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+var jwt = require('jsonwebtoken');
+var { expressjwt: jwt } = require("express-jwt");
 
 exports.signup = (req, res) => {
-    const errors = validationResult(req);
+
     const user = new User(req.body);
+    const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res
@@ -14,7 +17,7 @@ exports.signup = (req, res) => {
     user.save((err, user) => {
         if (err) {
             return res.status(400).json({
-                err: "not able to save user in DB",
+                err: "Not able to save user in DB",
             });
         }
         res.json({
@@ -30,3 +33,40 @@ exports.signout = (req, res) => {
         message: "User sign out",
     });
 };
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res
+            .status(422)
+            .json({ error: errors.array()[0].msg, param: errors.array()[0].param });
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return res.status(400).json({
+                err: "Not able to find user with given email in DB",
+            });
+        }
+
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                err: "Email and password do not match.",
+            });
+        }
+
+        //create token
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+        //put token in cookie
+        res.cookie("token", token, { expire: new Date() + 9999 });
+
+        //send response to front end
+        const { _id, name, email, role } = user;
+        res.json({ token, user: { _id, name, email, role } });
+    });
+}
+
